72bbee978465efb0eb90430dd7fd471e20008485
yusuketomoto <yukmpkt@gmail.com>
2016-12-16 17:08:15 +0900

    Enable to transfer only style but not color.

--- generate.py
+++ generate.py
@@ -16,8 +16,15 @@ parser.add_argument('--model', '-m', default='models/style.model', type=str)
 parser.add_argument('--out', '-o', default='out.jpg', type=str)
 parser.add_argument('--median_filter', default=3, type=int)
 parser.add_argument('--padding', default=50, type=int)
-
+parser.add_argument('--keep_colors', action='store_true')
+parser.set_defaults(keep_colors=False)
 args = parser.parse_args()
+
+# from 6o6o's fork. https://github.com/6o6o/chainer-fast-neuralstyle/blob/master/generate.py
+def original_colors(original, stylized):
+    h, s, v = original.convert('HSV').split()
+    hs, ss, vs = stylized.convert('HSV').split()
+    return Image.merge('HSV', (h, s, vs)).convert('RGB')
 
 model = FastStyleNet()
 serializers.load_npz(args.model, model)
@@ -27,7 +34,8 @@ if args.gpu >= 0:
 xp = np if args.gpu < 0 else cuda.cupy
 
 start = time.time()
-image = np.asarray(Image.open(args.input).convert('RGB'), dtype=np.float32).transpose(2, 0, 1)
+original = Image.open(args.input).convert('RGB')
+image = np.asarray(original, dtype=np.float32).transpose(2, 0, 1)
 image = image.reshape((1,) + image.shape)
 if args.padding > 0:
 	image = np.pad(image, [[0, 0], [0, 0], [args.padding, args.padding], [args.padding, args.padding]], 'symmetric')
@@ -43,6 +51,8 @@ result = np.uint8(result[0].transpose((1, 2, 0)))
 med = Image.fromarray(result)
 if args.median_filter > 0:
 	med = med.filter(ImageFilter.MedianFilter(args.median_filter))
+if args.keep_colors:
+    med = original_colors(original, med)
 print(time.time() - start, 'sec')
 
 med.save(args.out)
